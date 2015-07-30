8fe388d494f64152b9aa3037c77f05dec880d091
Johny M. Salas M <johmsalas@gmail.com>
2015-07-30 12:58:10 -0500

    Optimizations
    + cache
    + int to short

--- problems/100 - The 3n + 1 problem/solution.cpp
+++ problems/100 - The 3n + 1 problem/solution.cpp
@@ -10,34 +10,39 @@
 #include <algorithm>
 using namespace std;
 
-int cyclelength(unsigned int n) {
+// The maximum size was calculating by executing the algorithm from 1 to 10000
+//  and watching all possibles n
+#define SIZE 13557212
 
-	unsigned int length = 0;
+static unsigned short cache[SIZE];
 
-	while(n > 1) {
-		if( n%2 == 1) {
-			// Instead of n = 3 * n + 1, let's do this step
-			// and the next one (n/2), given that after every
-			// 3n+1 there is always an n/2
-			n = 1.5 * n + 0.5;
-			length = length + 2;
-		} else {
-			n = n / 2;
-			length++;
-		}
+unsigned short cyclelength(unsigned int n) {
+
+	// If the number was already calculated, return last result
+	if(n < SIZE && cache[n])
+		return cache[n];
+
+	if( n%2 == 1) {
+		// Instead of n = 3 * n + 1, let's do this both step in one
+		// and the next one (n/2), given that after every
+		// 3n+1 there is always an n/2
+		cache[n] = 2 + cyclelength(1.5 * n + 0.5);
+	} else {
+		cache[n] = 1 + cyclelength(n / 2);
 	}
 
-	return (length+1);
+	return cache[n];
 
 }
 
-unsigned int maxcycleLengthBetween(unsigned int i, unsigned int j) {
+unsigned short maxcycleLengthBetween(unsigned int i, unsigned int j) {
 
+	//j can be smaller than i
 	if(i > j) swap(i, j);
 
-	unsigned int max = 1;
+	unsigned short max = 1;
 	for(unsigned int n = i; n <= j; n++) {
-		unsigned int cl = cyclelength(n);
+		unsigned short cl = cyclelength(n);
 		if( cl > max ) {
 			max = cl;
 		}
@@ -48,9 +53,10 @@ unsigned int maxcycleLengthBetween(unsigned int i, unsigned int j) {
 int main() {
 
 	int i, j;
+	cache[1] = 1;
 
 	while ( scanf ("%d %d", &i, &j) != EOF ) {
-		unsigned int max = maxcycleLengthBetween( i, j);
+		unsigned short max = maxcycleLengthBetween( i, j);
 		printf ("%d %d %d\n", i, j, max);
 	}
 	return 0;
